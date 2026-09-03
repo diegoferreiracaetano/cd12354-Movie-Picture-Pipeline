ffcbefb1d85b50fc488e8c8c12cd926288ad0a8b
Diego Caetano <diegofcaetano@gmail.com>
2026-09-03 18:48:08 -0300

    Bump AWS provider to ~>5.0 to support AL2023 EKS node group ami_type

--- setup/terraform/versions.tf
+++ setup/terraform/versions.tf
@@ -7,7 +7,8 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.55.0"
+      # 4.55.0 predates AL2023 ami_type support on aws_eks_node_group
+      version = "~> 5.0"
     }
   }
 }
